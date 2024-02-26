a5723a44acccb3112813f40f1484b9b5e696056e
Aiken Tine Ahac <ahac.aiken@gmail.com>
2024-02-26 21:52:00 +0100

    Responsive navigation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -33,11 +33,10 @@
 	<nav class="justify center mb-2 flex flex-col items-center">
 		<!-- Logo and language switcher -->
 		<div
-			class="flex w-screen flex-row items-center justify-between bg-[url('/decors/navigation_bg.png')] bg-cover p-4"
+			class="flex w-screen flex-col items-center justify-between bg-[url('/decors/navigation_bg.png')] bg-cover py-4"
 		>
-			<div class="w-24" />
 			<img src={logo} alt="Tinta Tattoo Logo" width="350" />
-			<div class="w-24">
+			<div class="mt-4 w-24">
 				<button on:click={() => setLocale('sl')} class="transition-all duration-150 hover:font-bold"
 					>SLO</button
 				>
@@ -49,12 +48,12 @@
 		</div>
 		<!-- Navigation links -->
 		<div
-			class="flex w-screen items-center justify-center border border-l-0 border-r-0 border-gray-400 p-2"
+			class="flex w-screen flex-row items-center justify-center border border-l-0 border-r-0 border-gray-400 p-2"
 		>
 			<div class="flex w-10/12 flex-row items-center justify-evenly px-24 text-gray-700">
 				{#each routes as route, i}
 					<a
-						class="transition-all duration-150 hover:font-bold"
+						class="text-center transition-all duration-150 hover:font-bold"
 						href={isEng ? route.linkEng : route.link}>{isEng ? route.nameEng : route.name}</a
 					>
 					{#if i < routes.length - 1}
