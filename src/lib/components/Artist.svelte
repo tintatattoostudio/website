da902a9e2afefa5732d77b36e7c1f2223537547c
Aiken Tine Ahac <ahac.aiken@gmail.com>
2024-03-18 17:06:29 +0100

    Artist gallery preview ob home page

--- src/lib/components/Artist.svelte
+++ src/lib/components/Artist.svelte
@@ -2,7 +2,9 @@
 	import { goto } from '$app/navigation';
 	import API from '$lib/api/client';
 	import type { Artist } from '$lib/api/types';
+	import { Carousel } from 'flowbite-svelte';
 	import { onMount } from 'svelte';
+	import type { HTMLImgAttributes } from 'svelte/elements';
 
 	onMount(() => {
 		const current = window.localStorage.getItem('locale');
@@ -14,21 +16,53 @@
 		}
 	});
 
+	function getGalleryPreview() {
+		const images: HTMLImgAttributes[] = [];
+
+		artist.attributes.images?.data.slice(0, 5).forEach((img, index) => {
+			const image: HTMLImgAttributes = {
+				alt: `Preview ${index}`,
+				src: API.getImageUrl(img.attributes.url),
+				title: `Preview ${index}`,
+			};
+
+			images.push(image);
+		});
+
+		return images;
+	}
+
 	let isEng = false;
 	export let artist: Artist;
 </script>
 
-<a
-	href={`/${isEng ? 'en' : 'sl'}/gallery/${artist.attributes.artist}`}
-	class="flex flex-col items-center justify-center gap-2 transition-all duration-200 hover:-translate-y-1 hover:scale-105 hover:cursor-pointer hover:opacity-80"
->
-	<img
-		class="h-52 w-52 rounded-full object-cover"
-		src={API.getImageUrl(artist.attributes.pfp.data.attributes.url)}
-		alt={artist.attributes.name}
-	/>
-	<div class="flex flex-col items-center justify-center">
-		<p class="text-2xl font-bold text-offblack">{artist.attributes.name}</p>
-		<p class="text-offblack">{artist.attributes.styles}</p>
+<div>
+	<a
+		href={`/${isEng ? 'en' : 'sl'}/gallery/${artist.attributes.artist}`}
+		class="mb-4 flex flex-col items-center justify-center gap-2 transition-all duration-200 hover:-translate-y-1 hover:scale-105 hover:cursor-pointer hover:opacity-80"
+	>
+		<img
+			class="h-52 w-52 rounded-full object-cover"
+			src={API.getImageUrl(artist.attributes.pfp.data.attributes.url)}
+			alt={artist.attributes.name}
+		/>
+		<div class="flex flex-col items-center justify-center">
+			<p class="text-2xl font-bold text-offblack">{artist.attributes.name}</p>
+			<p class="text-offblack">{artist.attributes.styles}</p>
+		</div>
+	</a>
+	<div>
+		<Carousel
+			class="h-64 sm:h-64 xl:h-96 2xl:h-96"
+			images={getGalleryPreview()}
+			imgClass="h-full"
+			let:Controls
+			let:Indicators
+			transition={null}
+			duration={10000}
+		>
+			<Indicators />
+			<Controls />
+		</Carousel>
 	</div>
-</a>
+</div>
