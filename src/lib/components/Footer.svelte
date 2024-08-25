426409ba358806817d4277c3de60d55b49900cb4
Aiken Tine Ahac <30961404+aikenahac@users.noreply.github.com>
2024-08-25 10:31:39 +0200

    Update map in footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -34,7 +34,7 @@
 	<div class="px-20">
 		<iframe
 			title="Tattoo Studio Location"
-			src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2768.177745627873!2d14.470670277030784!3d46.06749689298307!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x477acd5200d57a59%3A0xd614356aea33f419!2sKaringerjeva%20ulica%208%2C%201000%20Ljubljana!5e0!3m2!1sen!2ssi!4v1704786011626!5m2!1sen!2ssi"
+			src="https://maps.google.com/maps?hl=en&amp;q=Trubarjeva%20cesta%2028,%201000%20Ljubljana+(Tinta%20Tattoo%20Studio)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
 			style="border:0;"
 			class="h-[450px] w-[350px] rounded-lg shadow-xl max-[650px]:w-[640px]"
 			allowfullscreen={false}
