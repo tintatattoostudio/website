9d54cc0964062020c2d1b1ca9134285f3d2d3398
Aiken Tine Ahac <ahac.aiken@gmail.com>
2024-10-27 08:25:09 +0100

    Restructure client

--- src/lib/api/client.ts
+++ src/lib/api/client.ts
@@ -17,56 +17,54 @@ const axiosInstance = Axios.create({
 	baseURL: `${PUBLIC_API_URL}/api`,
 });
 
-module API {
-	export function getImageUrl(url: string | undefined) {
+export default class API {
+	static getImageUrl(url: string | undefined) {
 		if (!url) return '';
-
+	
 		return PUBLIC_API_URL + url;
 	}
-
-	export async function getArtistGallery(artist: string, en: boolean) {
+	
+	static async getArtistGallery(artist: string, en: boolean) {
 		const locale = en ? '&locale=en' : '';
 		const data = await axiosInstance.get<{ data: Artist[] }>(
 			`/galleries?filters[artist][$eq]=${artist}&populate=deep${locale}`,
 		);
-
+	
 		return data.data.data.at(0);
 	}
-
-	export async function getFAQ(en: boolean): Promise<FAQSection[]> {
+	
+	static async getFAQ(en: boolean): Promise<FAQSection[]> {
 		const locale = en ? '&locale=en' : '';
 		const data = await axiosInstance.get<FAQ>(`/faq?populate=deep${locale}`);
-
+	
 		return data.data.data.attributes.section;
 	}
-
-	export async function getCare(en: boolean): Promise<Care> {
+	
+	static async getCare(en: boolean): Promise<Care> {
 		const locale = en ? '&locale=en' : '';
 		const data = await axiosInstance.get<CareBase>(`/care?populate=deep${locale}`);
-
+	
 		return data.data.data.attributes;
 	}
-
-	export async function getInstructions(en: boolean): Promise<Instructions> {
+	
+	static async getInstructions(en: boolean): Promise<Instructions> {
 		const locale = en ? '&locale=en' : '';
 		const data = await axiosInstance.get<InstructionsBase>(`/pretattoo?populate=deep${locale}`);
-
+	
 		return data.data.data.attributes;
 	}
-
-	export async function getLanding(en: boolean): Promise<Landing> {
+	
+	static async getLanding(en: boolean): Promise<Landing> {
 		const locale = en ? '?locale=en' : '';
 		const data = await axiosInstance.get<LandingBase>(`/landing${locale}`);
-
+	
 		return data.data.data.attributes;
 	}
-
-	export async function getArtists(en: boolean): Promise<Artist[]> {
+	
+	static async getArtists(en: boolean): Promise<Artist[]> {
 		const locale = en ? '&locale=en' : '';
 		const data = await axiosInstance.get<{ data: Artist[] }>(`/galleries?populate=deep${locale}`);
-
+	
 		return data.data.data;
-	}
+	}	
 }
-
-export default API;
